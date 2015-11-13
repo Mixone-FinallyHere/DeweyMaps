d26984f4eb13a8201c9c9fe6f5f82ccb19e7720d
brochape <brochape@ulb.ac.be>
2015-11-13 11:38:31 +0100

    The cursor is now a crosshair on the map when adding a marker

--- static/functions.js
+++ static/functions.js
@@ -114,9 +114,11 @@ var submit_form = function() {
 
 var error_suggest = function() {
   $('#errorsuggest-dropdown').removeClass("f-dropdown");
+  $('.leaflet-container').css('cursor','crosshair');
 
   map.once('click', function(e) {
     $('#errorsuggest-dropdown').addClass("f-dropdown");
+    $('.leaflet-container').css('cursor','');
     $('#addPointModal').foundation('reveal', 'open');
     $('input[name=lat]').val(e.latlng.lat);
     $('input[name=lon]').val(e.latlng.lng);
