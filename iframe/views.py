a08a269e5d3ee184654d1a4191496a33937e5b69
Nikita Marchant <nikita.marchant@gmail.com>
2015-12-17 16:08:35 +0100

    More design

--- iframe/views.py
+++ iframe/views.py
@@ -5,6 +5,7 @@ from django.views.generic import FormView
 from iframe.serializers import MapSerializer
 from rest_framework import viewsets
 from iframe.forms import MapForm
+from closet.models import Category
 
 from django.views.decorators.clickjacking import xframe_options_exempt
 
@@ -26,3 +27,12 @@ class IframeViewSet(viewsets.ModelViewSet):
 class IframeFormView(FormView):
     form_class = MapForm
     template_name = "iframe/map_form.html"
+
+    def post(self, request, *args, **kwargs):
+        import ipdb; ipdb.set_trace()
+        return super(IframeFormView, self).post(self, request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(IframeFormView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
